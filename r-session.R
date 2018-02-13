482976cee76eff2157ee8d962ae362eb536d3ab2
Soedomoto <soedomoto@gmail.com>
2018-02-13 20:51:40 +0700

    Add r-session

---
+++ r-session.R
@@ -0,0 +1,43 @@
+args <- commandArgs(trailingOnly=TRUE);
+if (length(args)==0) {
+  stop("First argument as \'port\' must be supplied.", call.=FALSE)
+}
+
+port <- args[1];
+
+library(rzmq)
+
+context = init.context()
+socket = init.socket(context, "ZMQ_REP")
+bind.socket(socket, paste("tcp://*:", port, sep=""))
+
+cat('[', port, ']', ' Session server is listening on port ', port, '\n');
+
+while(1) {
+    msg = receive.socket(socket);
+    cmd <- msg$cmd;
+    func <- msg$func;
+    args <- msg$args;    
+    resp <- NULL;
+
+    print(cmd)
+
+    if (!is.null(cmd)) { 
+        if (grepl('<-', cmd)) {
+            cat('[', port, ']', 'Executing command', cmd, '\n');
+
+            eval(parse(text=cmd));
+            resp <- 'OK';
+        } else {
+            cat('[', port, ']', 'Evaluating command', cmd, '\n');
+            resp <- eval(parse(text=cmd));
+        }
+    } 
+
+    else if (!is.null(func)) {
+        cat('[', port, ']', 'Calling function', func, '(', paste(args, collapse = ''), ')', '\n');
+        resp <- do.call(func, args);
+    }
+    
+    send.socket(socket, resp);
+}
